3bce3115e39a38811a71216b04fdd0a5c532c73c
LongTuyen <longtuyen05cntt1@gmail.com>
2011-10-20 16:45:48 +0700

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AppDemoSQLite::Application.routes.draw do
   resources :posts
-	root :to => "layouts#application"
+	root :to => "posts#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
